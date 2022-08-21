5bd379d4c734e24a027a6cedd5d9d35362dca39c
Alexander Podstrechnyy <tankalxat34@gmail.com>
2022-08-21 14:08:29 +0300

    Сделана установка и удаление пакетов

--- launch.py
+++ launch.py
@@ -6,6 +6,8 @@ Launch the pipios console
 
 import pathlib
 import os
+from io import BytesIO
+import shutil
 import zipfile
 import urllib
 import urllib.request
@@ -46,18 +48,29 @@ def _installPackage(package: str):
             print("Python version does not appear for installing")
 
         if python_version in [True, None]:
-            print("Installing", response["filename"], end=" ")
-            # fileForInstall = urllib.request.urlretrieve(response["url"], response["filename"])
-            print("Completed!")
+            print("Installing", response["filename"])            
+            whl_archive = BytesIO(urllib.request.urlopen(response["url"]).read())
 
-            with zipfile.ZipFile(response["filename"], mode="r") as archive:
+            with zipfile.ZipFile(file=whl_archive, mode="r") as archive:
                 for file in archive.namelist():
-                    if file.split("/")[0] == pypi_response["info"]["name"]:
-                        archive.extract(file)
+                    if "/" in file:
+                        if file.split("/")[0] == package:
+                            archive.extract(file)
+                        else:
+                            break
                     else:
-                        break
+                        archive.extract(file)
+            print(f"The \"{package}\" package was installed successfully!")
     else:
         print(f"Package \"{package}\" does not existing on PyPi!")
+
+
+def _deletePackage(package: str):
+    try:
+        shutil.rmtree(package)
+        print(f"The \"{package}\" package was removed successfully!")
+    except FileNotFoundError:
+        print(f"The \"{package}\" package does not existing!")
 
 
 def _getPyPiPackageInfo(package: str):
@@ -72,25 +85,25 @@ def _getPyPiPackageInfo(package: str):
 
 COMMANDS = {
     "pipios": lambda: print("PIPIOS - this is a tool for Pythonista for full managing your installed Python-packages. Type help to get more information.", sys.platform),
-    "exit": lambda: quit(),
+    "exit": lambda: sys.exit(),
     "package": lambda c: print(c),
-    "install": lambda c: _installPackage(c.split()[1])
+    "install": lambda c: _installPackage(c.split()[1]),
+    "i": lambda c: _installPackage(c.split()[1]),
+    "d": lambda c: _deletePackage(c.split()[1])
 }
 
 # command = "install uploadgrampyapi"
 # command = "install vk_api"
-command = "install requests"
-COMMANDS[command.split(" ")[0]](command)
-
+# command = "install requests"
+command = "pipios"
 # command = "pipios"
-# COMMANDS[command]()
+COMMANDS[command]()
 
-# while command != "exit":
-#     try:
-#         command = str(input(">>>"))
-#     except TypeError:
-#         pass
-#     try:
-#         COMMANDS[command.split()[0]](command)
-#     except KeyError:
-#         command = str(input("Invalid command. Type help to get more information...\n>>>"))
+while command != "exit":
+
+    command = str(input(">>>"))
+
+    try:
+        COMMANDS[command.split()[0]](command)
+    except KeyError:
+        command = str(input("Invalid command. Type help to get more information...\n>>>"))
